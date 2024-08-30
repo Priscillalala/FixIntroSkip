eb858038cbdb7562364f0fcfdbfaca6c66026855
Priscillalala <95263024+Priscillalala@users.noreply.github.com>
2024-08-30 13:49:51 -0700

    clean up code

--- FixIntroSkip/FixIntroSkipPlugin.cs
+++ FixIntroSkip/FixIntroSkipPlugin.cs
@@ -2,16 +2,14 @@
 using System.Security.Permissions;
 using System.Security;
 using BepInEx;
-using System.Linq;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using System.Collections;
 using RoR2;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
-[assembly: SecurityPermission(System.Security.Permissions.SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618 // Type or member is obsolete
 [assembly: HG.Reflection.SearchableAttribute.OptIn]
 
@@ -30,26 +28,9 @@ namespace FixIntroSkip
 
         public void Awake()
         {
-            //On.RoR2.SplashScreenController.Start += SplashScreenController_Start;
-            On.RoR2.RoR2Application.PrintSW += RoR2Application_PrintSW;
             On.RoR2.SplashScreenController.Finish += SplashScreenController_Finish;
             On.LoadHelper.LoadSceneAdditivelyAsync += LoadHelper_LoadSceneAdditivelyAsync;
             On.LoadHelper.StartLoadSceneAdditiveAsync += LoadHelper_StartLoadSceneAdditiveAsync;
-        }
-
-        private void SplashScreenController_Start(On.RoR2.SplashScreenController.orig_Start orig, SplashScreenController self)
-        {
-            orig(self);
-            if (SplashScreenController.cvSplashSkip.value)
-            {
-                SceneManager.UnloadSceneAsync(self.gameObject.scene);
-            }
-        }
-
-        private void RoR2Application_PrintSW(On.RoR2.RoR2Application.orig_PrintSW orig, string message)
-        {
-            orig(message);
-            Debug.Log(message);
         }
 
         private void SplashScreenController_Finish(On.RoR2.SplashScreenController.orig_Finish orig, SplashScreenController self)
@@ -70,61 +51,13 @@ namespace FixIntroSkip
         {
             if (_sceneGuid == SPLASH_SCENE_GUID && SplashScreenController.cvSplashSkip.value)
             {
-                /*if (IntroCutsceneController.shouldSkip)
-                {
-                    if (RoR2Application.isLoading)
-                    {
-                        RoR2Application.onLoad = (Action)Delegate.Combine(RoR2Application.onLoad, delegate ()
-                        {
-                            PlatformSystems.networkManager.ServerChangeScene("title");
-                        });
-                    }
-                    else
-                    {
-                        PlatformSystems.networkManager.ServerChangeScene("title");
-                    }
-                }
-                else
-                {
-                    RoR2Application.instance.StartCoroutine(LoadHelper.LoadSceneAdditivelyAsync(INTRO_SCENE_GUID));
-                }*/
-
-                
-                
-                //return Enumerable.Empty<object>().GetEnumerator();
                 static IEnumerator FinishSplashScreen()
                 {
-
                     GameObject splash = new GameObject("Splash");
                     splash.SetActive(false);
                     SplashScreenController splashScreenController = splash.AddComponent<SplashScreenController>();
                     splash.SetActive(true);
                     splashScreenController.Finish();
-                    /*if (RoR2Application.isLoading)
-                    {
-                        DontDestroyOnLoad(splash);
-                        RoR2Application.onLoad = (Action)Delegate.Combine(RoR2Application.onLoad, delegate ()
-                        {
-                            Destroy(splash);
-                        });
-                    }*/
-                    /*if (!IntroCutsceneController.shouldSkip)
-                    {
-                        RoR2Application.instance.StartCoroutine(LoadHelper.LoadSceneAdditivelyAsync("330d792ae1727574e969e68ce8e966d2"));
-                        yield break;
-                    }
-
-                    if (RoR2Application.isLoading)
-                    {
-                        RoR2Application.onLoad = (Action)Delegate.Combine(RoR2Application.onLoad, delegate ()
-                        {
-                            PlatformSystems.networkManager.ServerChangeScene("title");
-                        });
-                    }
-                    else
-                    {
-                        PlatformSystems.networkManager.ServerChangeScene("title");
-                    }*/
                     yield break;
                 }
                 return FinishSplashScreen();
@@ -135,7 +68,7 @@ namespace FixIntroSkip
         private AsyncOperationHandle LoadHelper_StartLoadSceneAdditiveAsync(On.LoadHelper.orig_StartLoadSceneAdditiveAsync orig, string _sceneGuid)
         {
             // this seems to always brick the game if left unattended
-            if (_sceneGuid == INTRO_SCENE_GUID && RoR2Application.isLoading)// && IntroCutsceneController.shouldSkip && RoR2Application.isLoading)
+            if (_sceneGuid == INTRO_SCENE_GUID && RoR2Application.isLoading)
             {
                 return default;
             }
